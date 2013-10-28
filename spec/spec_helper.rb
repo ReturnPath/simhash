fc3de8989768884ad0974f5967edc3e264db76ff
Ryan Michael <kerinin@gmail.com>
2013-10-28 17:27:53 -0500

    Resolve test issues and an import failure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,4 @@
 require 'rubygems'
 require 'rspec'
-require 'unicode'
 
 require_relative "../lib/simhash"
-require_relative "../lib/string_hasher"
-require_relative "../lib/integer"
-
-begin
-  require_relative "../string_hashing" 
-rescue LoadError
-  nil
-end
